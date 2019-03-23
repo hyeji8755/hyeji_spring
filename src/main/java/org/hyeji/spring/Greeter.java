bfc1eb7903b02d257aefa877beaeacfb3750284f
hyeji <hyeji8755@hanmail.net>
2019-03-24 01:16:33 +0900

    hyeji

---
+++ src/main/java/org/hyeji/spring/Greeter.java
@@ -0,0 +1,10 @@
+package org.hyeji.spring;
+
+public class Greeter {
+	public Greeter() {
+		System.out.println("Greeter instance created!");
+	}
+	public String greet() {
+		return "Hello, World!";
+	}
+}
